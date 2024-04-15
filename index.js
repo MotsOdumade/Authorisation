ff4101bf2fd8badd1d5e39dd8db86acbcd0e06f7
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-15 06:54:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const express = require('express');
 const requestIp = require('request-ip');
 const app = express();
 
-const PORT = 3004; // Port for Authorisation API
+const PORT = 3004; // Port for Authorisation API - we'll only enable localhost access
 
 // Middleware to get client IP address
 app.use(requestIp.mw());
 
 app.get('/v1/authorisation', (req, res) => {
     const remoteAddress = req.connection.remoteAddress;
-    // Check if the request is coming from localhost or specific IP addresses
+// ------ for localhost clients (APIs) we provide an authorisation service
     if (remoteAddress === '::ffff:127.0.0.1' || remoteAddress === '::1') {
-        // Allow the request to proceed - localhost clients only!
-        res.send("connected to the internal API, yay");
+// ------ apply logic to check if the user is authorised to make their request
+        const responseObj = {
+            'cacheable': false,
+            'authorised': true
+        };
+        res.json(responseObj);
     } else {
+// ------ don't serve external clients
         // Reject the request with a 403 Forbidden response
         res.status(403).send('Forbidden');
     }
