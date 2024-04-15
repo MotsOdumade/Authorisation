afb2833c8a4851f97e79a40b9dec76ef3f91a789
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-15 04:46:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const PORT = 3004; // Port for Authorisation API
 app.use(requestIp.mw());
 
 app.get('/v1/authorisation', (req, res) => {
-    const remoteAddress = req.clientIp;
+    const remoteAddress = req.connection.remoteAddress;
     // Check if the request is coming from localhost or specific IP addresses
-    if (remoteAddress === '127.0.0.1' || remoteAddress === '::1') {
+    if (remoteAddress === '::ffff:127.0.0.1' || remoteAddress === '::1') {
         // Allow the request to proceed - localhost clients only!
         res.send("connected to the internal API, yay");
     } else {
         // Reject the request with a 403 Forbidden response
         res.status(403).send('Forbidden');
-        //res.send(req.originalUrl);
     }
 });
 
